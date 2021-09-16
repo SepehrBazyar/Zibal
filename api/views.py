17b1b398d5d85f127117367ea0f60067d879c89f
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-09-16 23:21:41 +0430

    add url and view for get params to query in mongo

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Transaction
 
 # Create your views here.
+class TransactionDetailAPIView(APIView):
+    """
+    API View for Show Detail of Transactions
+    """
+
+    def get(self, request, format=None):
+        GET = request.GET
+        return Response(
+            Transaction.result(GET['type'], GET['mode'], GET.get('merchantId', None)),
+            status=status.HTTP_200_OK
+        )
